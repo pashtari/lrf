25bae3759f9471b6eeb875ab167e2684aec5dd1f
Pourya <p.behmandpoor@gmail.com>
2024-01-30 18:04:31 +0100

    modified directories

--- models/resnet/scripts/model.py
+++ models/resnet/scripts/model.py
@@ -11,17 +11,25 @@ class IMGResNet(nn.Module):
         self,
         num_classes=10,
         compression_ratio=1,
+        rescale=True,
+        image_size=(224,224),
     ):
         super(IMGResNet, self).__init__()
 
         self.num_classes = num_classes
         self.compression_ratio = _pair(compression_ratio)
+        self.image_size = _pair(image_size)
+        self.rescale = rescale
         self.model = resnet50()
 
     def forward(self, x):
-        cr_sqrt = math.sqrt(random.uniform(*self.compression_ratio))
 
-        x = interpolate(x, scale_factor=1/cr_sqrt)
+        if self.compression_ratio > 1:
+            cr_sqrt = math.sqrt(random.uniform(*self.compression_ratio))
+            x = interpolate(x, scale_factor=1/cr_sqrt)
+            if self.rescale:
+                x = interpolate(x, size=self.image_size)
+        
         x = self.model(x)
 
         return x
