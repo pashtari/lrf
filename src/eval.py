0bb5dcdbb1bfd2668a9007439f2001abdd8f6491
Pourya <p.behmandpoor@gmail.com>
2024-01-31 11:20:06 +0100

    added eval.py & svdresnet.configs.eval.yaml

---
+++ src/eval.py
@@ -0,0 +1,48 @@
+import hydra
+import torch
+import rootutils
+
+from omegaconf import DictConfig
+from ignite.engine import (
+    Events,
+    create_supervised_evaluator,
+)
+from ignite.handlers import Checkpoint
+from ignite.contrib.handlers import TensorboardLogger
+
+rootutils.set_root(path=rootutils.find_root(search_from=__file__), pythonpath=True, cwd=True)
+
+@hydra.main(
+    version_base=None, config_path="../models/svdresnet/configs", config_name="eval"
+)
+def main(cfg: DictConfig) -> None:
+    device = cfg.device
+    model = hydra.utils.instantiate(cfg.model).to(device)
+    checkpoint_fp = "../checkpoints" + cfg.checkpoint
+    checkpoint = torch.load(checkpoint_fp, map_location=device) 
+    Checkpoint.load_objects(to_load={"model": model}, checkpoint=checkpoint) 
+
+    val_loader = hydra.utils.instantiate(cfg.val_loader)
+    val_metrics = {k: hydra.utils.instantiate(v) for k, v in cfg.val_metrics.items()}
+    evaluator = create_supervised_evaluator(model, val_metrics, device)
+
+    @evaluator.on(Events.COMPLETED)
+    def log_test_results(engine):
+        metrics = engine.state.metrics
+        print(
+            f"Test Results - Avg accuracy: {metrics['accuracy']:.2f} Avg loss: {metrics['loss']:.2f}"
+        )
+
+    tb_logger = TensorboardLogger(log_dir="tb-test-logger")
+    tb_logger.attach_output_handler(
+        evaluator,
+        event_name=Events.COMPLETED,
+        tag="test",
+        metric_names=["accuracy"],
+    )
+
+    evaluator.run(val_loader)
+    tb_logger.close()
+
+if __name__ == "__main__":
+    main()
--- src/train.py
+++ src/train.py
@@ -1,5 +1,5 @@
 import rootutils
-import hydra 
+import hydra
 
 from omegaconf import DictConfig
 from ignite.engine import (
@@ -22,15 +22,11 @@ def main(cfg: DictConfig) -> None:
     train_loader = hydra.utils.instantiate(cfg.train_loader)
     val_loader = hydra.utils.instantiate(cfg.val_loader)
 
-    device = cfg.device
-    model = hydra.utils.instantiate(cfg.model).to(device)
     optimizer = hydra.utils.instantiate(cfg.optimizer)(params=model.parameters())
     loss = hydra.utils.instantiate(cfg.loss)
-
-    trainer = create_supervised_trainer(model, optimizer, loss, device)
-
     val_metrics = {k: hydra.utils.instantiate(v) for k, v in cfg.val_metrics.items()}
 
+    trainer = create_supervised_trainer(model, optimizer, loss, device)
     train_evaluator = create_supervised_evaluator(model, val_metrics, device)
     val_evaluator = create_supervised_evaluator(model, val_metrics, device)
 
@@ -63,7 +59,6 @@ def main(cfg: DictConfig) -> None:
     val_evaluator.add_event_handler(Events.COMPLETED, model_checkpoint, {"model": model})
 
     tb_logger = TensorboardLogger(log_dir="tb-logger")
-
     tb_logger.attach_output_handler(
         trainer,
         event_name=Events.ITERATION_COMPLETED(every=cfg.log_interval),
