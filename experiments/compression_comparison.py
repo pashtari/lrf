ba15c00275bf155a60057a59c1103a1e2e868138
Pourya <p.behmandpoor@gmail.com>
2024-02-14 13:52:52 +0100

    added compression comparison for CIFAR10

--- experiments/compression_comparison.py
+++ experiments/compression_comparison.py
@@ -1,100 +1,10 @@
-import math
-
 import numpy as np
-import torch
 import matplotlib.pyplot as plt
 from skimage import data, img_as_float
 from skimage import exposure
 from skimage.transform import resize
-from skimage.metrics import structural_similarity
 
 from src.models import compression as com
-
-
-def zscore_normalize(tensor):
-    mean = np.mean(dim=(-2, -1), keepdims=True)
-    std = np.std(dim=(-2, -1), keepdims=True)
-    normalized_tensor = (tensor - mean) / (std + 1e-8)
-    return normalized_tensor
-
-
-def minmax_normalize(x):
-    min_val = torch.amin(x, dim=(-2, -1), keepdim=True)
-    max_val = torch.amax(x, dim=(-2, -1), keepdim=True)
-    normalized_tensor = (x - min_val) / (max_val - min_val)
-    return normalized_tensor
-
-
-def mae(image1, image2):
-    return np.mean(np.abs(image1 - image2))
-
-
-def mse(image1, image2):
-    return np.mean((image1 - image2) ** 2)
-
-
-def psnr(image1, image2):
-    mse_value = mse(image1, image2)
-    if mse_value == 0:
-        return float("inf")  # If the MSE is zero, the PSNR is infinite
-    max_pixel = np.max(image1)
-    psnr_value = 20 * math.log10(max_pixel / math.sqrt(mse_value))
-    return psnr_value
-
-
-def ssim(image1, image2):
-    ssim_value = structural_similarity(
-        image1, image2, data_range=image2.max() - image2.min(), channel_axis=-1
-    )
-    return ssim_value
-
-
-def compress_interpolate(x, compression_ratio):
-    """Compress the image by resizing using interpolation."""
-    x = torch.tensor(x, dtype=torch.float32).permute(-1, 0, 1).unsqueeze(0)
-    interpolate = com.Interpolate(mode="bilinear")
-    y = interpolate(x, compression_ratio=compression_ratio)
-    return torch.clip(y, 0, 1).squeeze(0).permute(1, 2, 0).to(torch.float64).numpy()
-
-
-def compress_interpolate_low(x, compression_ratio):
-    """Compress the image by resizing using interpolation."""
-    x = torch.tensor(x, dtype=torch.float32).permute(-1, 0, 1).unsqueeze(0)
-    interpolate = com.Interpolate(mode="bilinear")
-    y = interpolate.compress(x, compression_ratio=compression_ratio)
-    return torch.clip(y, 0, 1).squeeze(0).permute(1, 2, 0).to(torch.float64).numpy()
-
-
-def compress_dct(x, compression_ratio):
-    """Compress the image using DCT."""
-    x = torch.tensor(x, dtype=torch.float32).permute(-1, 0, 1).unsqueeze(0)
-    dct2 = com.DCT()
-    y = dct2(x, compression_ratio=compression_ratio)
-    return torch.clip(y, 0, 1).squeeze(0).permute(1, 2, 0).to(torch.float64).numpy()
-
-
-def compress_dct_low(x, compression_ratio):
-    """Compress the image using DCT."""
-    x = torch.tensor(x, dtype=torch.float32).permute(-1, 0, 1).unsqueeze(0)
-    dct2 = com.DCT()
-    y = dct2(x, compression_ratio=compression_ratio, pad=False)
-    return minmax_normalize(y).squeeze(0).permute(1, 2, 0).to(torch.float64).numpy()
-
-
-def compress_svd(x, compression_ratio):
-    """Compress the image using SVD."""
-    x = torch.tensor(x, dtype=torch.float32).permute(-1, 0, 1).unsqueeze(0)
-    svd = com.SVD()
-    y = svd(x, compression_ratio=compression_ratio)
-    return torch.clip(y, 0, 1).squeeze(0).permute(1, 2, 0).to(torch.float64).numpy()
-
-
-def compress_patch_svd(x, compression_ratio, patch_size=(8, 8)):
-    """Compress the image using SVD on patches."""
-    x = torch.tensor(x, dtype=torch.float32).permute(-1, 0, 1).unsqueeze(0)
-    patch_svd = com.PatchSVD(patch_size=patch_size)
-    y = patch_svd(x, compression_ratio=compression_ratio)
-    return torch.clip(y, 0, 1).squeeze(0).permute(1, 2, 0).to(torch.float64).numpy()
 
 
 # Load the astronaut image
@@ -110,7 +20,7 @@ plt.axis("off")
 plt.title("Astronaut Image")
 plt.show()
 
-for metric in [psnr, ssim, mae]:
+for metric in [com.psnr, com.ssim, com.mae]:
     # Compression ratios to be used
     compression_ratios = np.array([1.25, 1.5, 1.75, *np.arange(2, 25.5, 1)])
 
@@ -133,30 +43,30 @@ for metric in [psnr, ssim, mae]:
     # Calculate reconstructed images and metric for each method and compression ratio
     for ratio in compression_ratios:
         # Interpolate
-        compressed_interpolate = compress_interpolate(image, ratio)
+        compressed_interpolate = com.compress_interpolate(image, ratio)
         err_values["Interpolation"].append(metric(image, compressed_interpolate))
         compressed_images["Interpolation"].append(compressed_interpolate)
 
         # Interpolate low
-        compressed_interpolate_low = compress_interpolate(image, ratio)
+        compressed_interpolate_low = com.compress_interpolate_low(image, ratio)
         compressed_images["Interpolation Low"].append(compressed_interpolate_low)
 
         # DCT
-        compressed_dct = compress_dct(image, ratio)
+        compressed_dct = com.compress_dct(image, ratio)
         err_values["DCT"].append(metric(image, compressed_dct))
         compressed_images["DCT"].append(compressed_dct)
 
         # DCT Low
-        compressed_dct_low = compress_dct_low(image, ratio)
+        compressed_dct_low = com.compress_dct_low(image, ratio)
         compressed_images["DCT Low"].append(compressed_dct_low)
 
         # # SVD
-        # compressed_svd = compress_svd(image, ratio)
+        # compressed_svd = ccom.ompress_svd(image, ratio)
         # err_values["SVD"].append(metric(image, compressed_svd))
         # compressed_images["SVD"].append(compressed_svd)
 
         # SVD Patches
-        compressed_patch_svd = compress_patch_svd(image, ratio)
+        compressed_patch_svd = com.compress_patch_svd(image, ratio)
         err_values["Patch SVD"].append(metric(image, compressed_patch_svd))
         compressed_images["Patch SVD"].append(compressed_patch_svd)
 
