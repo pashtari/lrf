8d98d5de57d27e5f18607360d8dba5b2f20caed6
Pooya <pooya.ash@gmail.com>
2024-02-20 22:56:40 +0100

    polish

--- test_bash/first_script.py
+++
@@ -1,2 +0,0 @@
-# first_script.py
-print("Hello/from/the/first/script!")
--- test_bash/mybash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run the first script and capture its output
-output="$(python3 first_script.py)"
-
-# Run the second script with modified arguments
-python3 second_script.py arg2 ${output}  arg3
