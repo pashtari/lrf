98708428e059d6a2bd3bccab75c25fce6b7ac139
Pourya <p.behmandpoor@gmail.com>
2024-05-01 10:49:10 +0200

    polish

--- experiments/studies_over_datasets/utils/utils.py
+++ experiments/studies_over_datasets/utils/utils.py
@@ -5,16 +5,15 @@ from copy import deepcopy
 from scipy.interpolate import interp1d
 import tikzplotlib
 import lrf
-import argparse
 import os
-import json
 import datetime
 import pickle
+from torch.nn.modules.utils import _pair
 
 
 def tikzplotlib_fix_ncols(obj):
     """
-    workaround for matplotlib 3.6 renamed legend's _ncol to _ncols, which breaks tikzplotlib
+    workaround for matplotlib >= 3.6 renamed legend's _ncol to _ncols, which breaks tikzplotlib
     """
     if hasattr(obj, "_ncols"):
         obj._ncol = obj._ncols
@@ -68,13 +67,15 @@ def plot_result(x_values, y_values, x_axis_fixed_values, plot_num, figure_data=N
 def calc_compression_metrics(dataloader, compression_ratios, bpps, psnr_values, ssim_values, args):
     """ Calculates reconstructed images and metric values for each image and each method in the method_list """
 
+    bounds = args.bounds if isinstance(args.bounds,tuple) else (-args.bounds, args.bounds-1)
+
     image_num = len(dataloader)
     for image_id, image in enumerate(dataloader):
         print(f"processing image {image_id + 1}/{image_num}", flush=True)
         image = image.squeeze()
 
         if "JPEG" in args.selected_methods:
-            for quality in range(0, 50, 1):
+            for quality in range(0, 60, 1):
                 enocoded = lrf.pil_encode(image, format="JPEG", quality=quality)
                 reconstructed = lrf.pil_decode(enocoded)
 
@@ -88,7 +89,7 @@ def calc_compression_metrics(dataloader, compression_ratios, bpps, psnr_values, 
                 ssim_values["JPEG"].append(lrf.ssim(image, reconstructed))
 
         if "SVD" in args.selected_methods:
-            for quality in np.linspace(0.0, 50, 50):
+            for quality in np.linspace(0.0, 7, 30):
                 enocoded = lrf.svd_encode(
                     image, quality=quality, patch=args.pachify, patch_size=args.patch_size, dtype=torch.int8
                 )
@@ -105,14 +106,14 @@ def calc_compression_metrics(dataloader, compression_ratios, bpps, psnr_values, 
 
 
         if "IMF-RGB" in args.selected_methods:
-            for quality in np.linspace(0.0, 50, 50):
+            for quality in np.linspace(0.0, 30, 50):
                 enocoded = lrf.imf_encode(
                     image,
                     color_space="RGB",
                     quality=quality,
                     patch=args.pachify,
-                    patch_size=args.patch_size,
-                    bounds=args.bounds,
+                    patch_size=_pair(args.patch_size),
+                    bounds=bounds,
                     dtype=torch.int8,
                     num_iters=10,
                     verbose=False,
@@ -137,8 +138,8 @@ def calc_compression_metrics(dataloader, compression_ratios, bpps, psnr_values, 
                     scale_factor=(0.5, 0.5),
                     quality=(quality, quality / 2, quality / 2),
                     patch=args.pachify,
-                    patch_size=args.patch_size,
-                    bounds=args.bounds,
+                    patch_size=_pair(args.patch_size),
+                    bounds=bounds,
                     dtype=torch.int8,
                     num_iters=args.num_iters,
                     verbose=False,
@@ -167,16 +168,3 @@ def make_result_dir(experiment_dir, args):
         pickle.dump(args, f)
 
     return result_dir
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(description="Compression comparison over dataset")
-    parser.add_argument("--data_dir", default="data/kodak", type=str)
-    parser.add_argument("--experiment_name", default="kodak", type=str)
-    parser.add_argument("--patch_size", default=(8,8), type=tuple)
-    parser.add_argument("--bounds", default=(-16,15), type=tuple)
-    parser.add_argument("--num_iters", default=10, type=int)
-    parser.add_argument("--pachify", default=True, type=bool)
-    parser.add_argument("--selected_methods", default=["JPEG","SVD","IMF-RGB","IMF-YCbCr"], type=list)
-    args = parser.parse_args()
-    return args
