f603fd0843e89f5d6f00777c0b110ca5050be0b7
Pourya <p.behmandpoor@gmail.com>
2024-05-07 12:16:56 +0200

    added part of ablation studies and comparisons

--- experiments/studies_over_datasets/hpc/slurm.sh
+++ experiments/studies_over_datasets/hpc/slurm.sh
@@ -9,7 +9,7 @@ cd $VSC_DATA/projects/lsvd/experiments/studies_over_datasets/hpc/jobs
 sbatch --account=lp_inspiremed --job-name=abl_patchsize --clusters=genius --nodes=1 --ntasks-per-node=2 --cpus-per-task=9 --time=12:00:00 ablation_patchsize.sh
 sbatch --account=lp_inspiremed --job-name=abl_iternum --clusters=genius --nodes=1 --ntasks-per-node=2 --cpus-per-task=9 --time=12:00:00 ablation_iternum.sh
 sbatch --account=lp_inspiremed --job-name=abl_bounds --clusters=genius --nodes=1 --ntasks-per-node=2 --cpus-per-task=9 --time=12:00:00 ablation_bounds.sh
-sbatch --account=lp_inspiremed --job-name=abl_colorspace --clusters=genius --nodes=1 --ntasks-per-node=2 --cpus-per-task=9 --time=12:00:00 ablation_colorspace.sh
+sbatch --account=lp_inspiremed --job-name=abl_colorspace --clusters=genius --nodes=1 --ntasks-per-node=2 --cpus-per-task=9 --time=4:00:00 ablation_colorspace.sh
 sbatch --account=lp_inspiremed --job-name=abl_patchification --clusters=genius --nodes=1 --ntasks-per-node=2 --cpus-per-task=9 --time=12:00:00 ablation_patchification_svd.sh
 
 # Comparison over datasets
@@ -18,6 +18,6 @@ sbatch --account=lp_inspiremed --job-name=cmp_clic --clusters=genius --nodes=1 -
 sbatch --account=lp_inspiremed --job-name=cmp_imgnet --clusters=genius --nodes=1 --ntasks-per-node=2 --cpus-per-task=9 --time=24:00:00 comparison_imagenet.sh
 
 # Comparison of classification performance over a subset of Imagenet
-sbatch --account=lp_inspiremed --job-name=clsi_IMF --clusters=genius --nodes=1 --ntasks-per-node=2 --cpus-per-task=4 --gres=gpu:1 --time=24:00:00 --partition=gpu_v100 IMF_imagenet_classification.sh
-sbatch --account=lp_inspiremed --job-name=clsi_SVD --clusters=genius --nodes=1 --ntasks-per-node=2 --cpus-per-task=4 --gres=gpu:1 --time=24:00:00 --partition=gpu_v100 SVD_imagenet_classification.sh
-sbatch --account=lp_inspiremed --job-name=clsi_JPG --clusters=genius --nodes=1 --ntasks-per-node=2 --cpus-per-task=4 --gres=gpu:1 --time=24:00:00 --partition=gpu_v100 JPEG_imagenet_classification.sh
+sbatch --account=lp_inspiremed --job-name=clsi_IMF --clusters=genius --nodes=1 --ntasks-per-node=1 --cpus-per-task=9 --gres=gpu:1 --time=24:00:00 --partition=gpu_p100 IMF_imagenet_classification.sh
+sbatch --account=lp_inspiremed --job-name=clsi_SVD --clusters=genius --nodes=1 --ntasks-per-node=1 --cpus-per-task=9 --gres=gpu:1 --time=24:00:00 --partition=gpu_p100 SVD_imagenet_classification.sh
+sbatch --account=lp_inspiremed --job-name=clsi_JPG --clusters=genius --nodes=1 --ntasks-per-node=1 --cpus-per-task=9 --gres=gpu:1 --time=24:00:00 --partition=gpu_p100 JPEG_imagenet_classification.sh
