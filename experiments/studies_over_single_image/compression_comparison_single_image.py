ba9199cbb31840ef5ad8a1f56984f4ffe0f3d6b8
Pourya <p.behmandpoor@gmail.com>
2024-05-16 19:00:42 +0200

    polish

--- experiments/studies_over_single_image/compression_comparison_single_image.py
+++ experiments/studies_over_single_image/compression_comparison_single_image.py
@@ -1,13 +1,30 @@
+import os
+import json
 import numpy as np
 import torch
 from torchvision.transforms import v2
+import matplotlib
 import matplotlib.pyplot as plt
 from skimage.io import imread
 
 import lrf
+matplotlib.use("pgf")
+matplotlib.rcParams.update({
+    "pgf.texsystem": "pdflatex",
+    'font.family': 'serif',
+    'pgf.rcfonts': False,
+})
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Load the image
-image = imread("./data/kodak/kodim23.png")
+task_name = "kodim19"
+image = imread("./data/kodak/kodim19.png")
+# image = imread("./data/clic/clic2024_validation_image/724b1dfdbde05257c46bb5e9863995b7c37bfcf101ed5a233ef2aa26193f09c4.png")
+
+task_dir = os.path.join(script_dir, task_name)
+if not os.path.exists(task_dir):
+    os.makedirs(task_dir)
 
 # Transform the input image
 transforms = v2.Compose([v2.ToImage()])
@@ -16,8 +33,14 @@ image = torch.tensor(transforms(image))
 # Visualize image
 plt.imshow(image.permute(1, 2, 0))
 plt.axis("off")
-plt.title("Original Image")
+# plt.title("Original Image", fontsize=24)
+plt.savefig(
+    f"{task_dir}/{task_name}_original.pdf",
+    format="pdf",
+    dpi=600,
+)
 plt.show()
+plt.close()
 
 
 # Store errors and compressed images for each method and CR/bpp
@@ -26,7 +49,7 @@ compression_ratios = {
     "WEBP": [],
     "SVD": [],
     "IMF - RGB": [],
-    "IMF - YCbCr": [],
+    "IMF": [],
 }
 
 bpps = {
@@ -34,7 +57,7 @@ bpps = {
     "WEBP": [],
     "SVD": [],
     "IMF - RGB": [],
-    "IMF - YCbCr": [],
+    "IMF": [],
 }
 
 reconstructed_images = {
@@ -42,7 +65,7 @@ reconstructed_images = {
     "WEBP": [],
     "SVD": [],
     "IMF - RGB": [],
-    "IMF - YCbCr": [],
+    "IMF": [],
 }
 
 psnr_values = {
@@ -50,21 +73,21 @@ psnr_values = {
     "WEBP": [],
     "SVD": [],
     "IMF - RGB": [],
-    "IMF - YCbCr": [],
+    "IMF": [],
 }
 ssim_values = {
     "JPEG": [],
     "WEBP": [],
     "SVD": [],
     "IMF - RGB": [],
-    "IMF - YCbCr": [],
+    "IMF": [],
 }
 
 # Calculate reconstructed images and metric values for each method
 
 
 # JPEG
-for quality in range(0, 80, 1):
+for quality in range(0, 40, 1):
     enocoded = lrf.pil_encode(image, format="JPEG", quality=quality)
     reconstructed = lrf.pil_decode(enocoded)
 
@@ -74,8 +97,8 @@ for quality in range(0, 80, 1):
     compression_ratios["JPEG"].append(real_compression_ratio)
     bpps["JPEG"].append(real_bpp)
     reconstructed_images["JPEG"].append(reconstructed)
-    psnr_values["JPEG"].append(lrf.psnr(image, reconstructed))
-    ssim_values["JPEG"].append(lrf.ssim(image, reconstructed))
+    psnr_values["JPEG"].append(lrf.psnr(image, reconstructed).item())
+    ssim_values["JPEG"].append(lrf.ssim(image, reconstructed).item())
 
 
 # # WebP
@@ -89,11 +112,11 @@ for quality in range(0, 80, 1):
 #     compression_ratios["WEBP"].append(real_compression_ratio)
 #     bpps["WEBP"].append(real_bpp)
 #     reconstructed_images["WEBP"].append(reconstructed)
-#     psnr_values["WEBP"].append(lrf.psnr(image, reconstructed))
-#     ssim_values["WEBP"].append(lrf.ssim(image, reconstructed))
+#     psnr_values["WEBP"].append(lrf.psnr(image, reconstructed).item())
+#     ssim_values["WEBP"].append(lrf.ssim(image, reconstructed).item())
 
 # SVD
-for quality in np.linspace(0.0, 7, 30):
+for quality in np.linspace(0.0, 5, 20):
     enocoded = lrf.svd_encode(
         image,
         color_space="RGB",
@@ -110,12 +133,12 @@ for quality in np.linspace(0.0, 7, 30):
     compression_ratios["SVD"].append(real_compression_ratio)
     bpps["SVD"].append(real_bpp)
     reconstructed_images["SVD"].append(reconstructed)
-    psnr_values["SVD"].append(lrf.psnr(image, reconstructed))
-    ssim_values["SVD"].append(lrf.ssim(image, reconstructed))
+    psnr_values["SVD"].append(lrf.psnr(image, reconstructed).item())
+    ssim_values["SVD"].append(lrf.ssim(image, reconstructed).item())
 
 
 # IMF - RGB
-for quality in np.linspace(0.0, 30, 50):
+for quality in np.linspace(0.0, 25, 20):
     enocoded = lrf.imf_encode(
         image,
         color_space="RGB",
@@ -135,12 +158,12 @@ for quality in np.linspace(0.0, 30, 50):
     compression_ratios["IMF - RGB"].append(real_compression_ratio)
     bpps["IMF - RGB"].append(real_bpp)
     reconstructed_images["IMF - RGB"].append(reconstructed)
-    psnr_values["IMF - RGB"].append(lrf.psnr(image, reconstructed))
-    ssim_values["IMF - RGB"].append(lrf.ssim(image, reconstructed))
+    psnr_values["IMF - RGB"].append(lrf.psnr(image, reconstructed).item())
+    ssim_values["IMF - RGB"].append(lrf.ssim(image, reconstructed).item())
 
 
 # IMF - YCbCr
-for quality in np.linspace(0, 50, 50):
+for quality in np.linspace(0, 25, 20):
     enocoded = lrf.imf_encode(
         image,
         color_space="YCbCr",
@@ -158,11 +181,11 @@ for quality in np.linspace(0, 50, 50):
     real_compression_ratio = lrf.get_compression_ratio(image, enocoded)
     real_bpp = lrf.get_bbp(image.shape[-2:], enocoded)
 
-    compression_ratios["IMF - YCbCr"].append(real_compression_ratio)
-    bpps["IMF - YCbCr"].append(real_bpp)
-    reconstructed_images["IMF - YCbCr"].append(reconstructed)
-    psnr_values["IMF - YCbCr"].append(lrf.psnr(image, reconstructed))
-    ssim_values["IMF - YCbCr"].append(lrf.ssim(image, reconstructed))
+    compression_ratios["IMF"].append(real_compression_ratio)
+    bpps["IMF"].append(real_bpp)
+    reconstructed_images["IMF"].append(reconstructed)
+    psnr_values["IMF"].append(lrf.psnr(image, reconstructed).item())
+    ssim_values["IMF"].append(lrf.ssim(image, reconstructed).item())
 
 
 selected_methods = [
@@ -170,88 +193,117 @@ selected_methods = [
     # "WEBP",
     "SVD",
     "IMF - RGB",
-    "IMF - YCbCr",
+    "IMF",
 ]
 bpps = {k: bpps[k] for k in selected_methods}
 reconstructed_images = {k: reconstructed_images[k] for k in selected_methods}
 psnr_values = {k: psnr_values[k] for k in selected_methods}
 ssim_values = {k: ssim_values[k] for k in selected_methods}
 
-# Plotting the results: PSNR vs bpp
-plt.figure()
-for method, values in psnr_values.items():
-    plt.plot(bpps[method], values, marker="o", markersize=4, label=method)
-
-plt.xlabel("bpp")
-plt.ylabel("PSNR (dB)")
-plt.title("Comprison of Different Compression Methods")
-# plt.xticks(np.arange(1, 13, 1))
-plt.xlim(0.05, 0.5)
-# plt.ylim(20, 30)
-plt.legend()
-plt.grid()
-plt.savefig(
-    "experiments/compression_methods_comparison_psnr-bpp.pdf",
-    format="pdf",
-    dpi=600,
-)
-plt.show()
+selected_bpps = [0.4, 0.3, 0.2, 0.15, 0.1]
 
 
-# Plotting the results: SSIM vs bpp
-plt.figure()
-for method, values in ssim_values.items():
-    plt.plot(bpps[method], values, marker="o", markersize=4, label=method)
+def plot_metrics_collage():
+    # Plotting the results: PSNR vs bpp
+    plt.figure()
+    for method, values in psnr_values.items():
+        plt.plot(bpps[method], values, marker="o", markersize=4, label=method)
 
-plt.xlabel("bpp")
-plt.ylabel("SSIM")
-plt.title("Comprison of Different Compression Methods")
-# plt.xticks(np.arange(1, 13, 1))
-plt.xlim(0.05, 0.5)
-# plt.ylim(0.5, 0.8)
-plt.legend()
-plt.grid()
-plt.savefig(
-    "experiments/compression_methods_comparison_ssim-bpp.pdf",
-    format="pdf",
-    dpi=600,
-)
-plt.show()
-
-
-# Plotting the compressed images for each method and bpp
-selected_bpps = [1, 0.75, 0.5, 0.4, 0.3, 0.2, 0.15, 0.1, 0.05]
-fig, axs = plt.subplots(
-    len(selected_bpps),
-    len(selected_methods),
-    figsize=(6 * len(selected_methods), 5 * len(selected_bpps)),
-)
-
-# Setting titles for columns
-for ax, method in zip(axs[0], selected_methods):
-    ax.set_title(method, fontsize=24)
-
-for i, bbp in enumerate(selected_bpps):
-    for j, method in enumerate(selected_methods):
-        ii = np.argmin(np.abs(np.array(bpps[method]) - bbp))
-        reconstructed_image = reconstructed_images[method][ii]
-        axs[i, j].imshow(reconstructed_image.permute(1, 2, 0))
-        real_bpp = bpps[method][ii]
-        axs[i, j].set_ylabel(f"bpp = {real_bpp:.2f}", rotation=90, fontsize=18)
-        axs[i, j].tick_params(
-            axis="both",
-            which="both",
-            bottom=False,
-            left=False,
-            labelbottom=False,
-            labelleft=False,
-        )
+    plt.xlabel("bpp")
+    plt.ylabel("PSNR (dB)")
+    plt.title("Comprison of Different Compression Methods")
+    # plt.xticks(np.arange(1, 13, 1))
+    plt.xlim(0.05, 0.5)
+    # plt.ylim(20, 30)
+    plt.legend()
+    plt.grid()
+    plt.savefig(
+        f"{task_dir}/compression_methods_comparison_psnr-bpp.pdf",
+        format="pdf",
+        dpi=600,
+    )
+    plt.show()
 
 
-plt.tight_layout()
-plt.savefig(
-    "experiments/compression_methods_qualitative_comparison.pdf",
-    format="pdf",
-    dpi=600,
-)
-plt.show()
+    # Plotting the results: SSIM vs bpp
+    plt.figure()
+    for method, values in ssim_values.items():
+        plt.plot(bpps[method], values, marker="o", markersize=4, label=method)
+
+    plt.xlabel("bpp")
+    plt.ylabel("SSIM")
+    plt.title("Comprison of Different Compression Methods")
+    # plt.xticks(np.arange(1, 13, 1))
+    plt.xlim(0.05, 0.5)
+    # plt.ylim(0.5, 0.8)
+    plt.legend()
+    plt.grid()
+    plt.savefig(
+        f"{task_dir}/compression_methods_comparison_ssim-bpp.pdf",
+        format="pdf",
+        dpi=600,
+    )
+    plt.show()
+
+
+    # Plotting the compressed images for each method and bpp
+    fig, axs = plt.subplots(
+        len(selected_bpps),
+        len(selected_methods),
+        figsize=(6 * len(selected_methods), 5 * len(selected_bpps)),
+    )
+
+    # Setting titles for columns
+    for ax, method in zip(axs[0], selected_methods):
+        ax.set_title(method, fontsize=24)
+
+    for i, bbp in enumerate(selected_bpps):
+        for j, method in enumerate(selected_methods):
+            ii = np.argmin(np.abs(np.array(bpps[method]) - bbp))
+            reconstructed_image = reconstructed_images[method][ii]
+            axs[i, j].imshow(reconstructed_image.permute(1, 2, 0))
+            real_bpp = bpps[method][ii]
+            axs[i, j].set_ylabel(f"bpp = {real_bpp:.2f}", rotation=90, fontsize=18)
+            axs[i, j].tick_params(
+                axis="both",
+                which="both",
+                bottom=False,
+                left=False,
+                labelbottom=False,
+                labelleft=False,
+            )
+
+    plt.tight_layout()
+    plt.savefig(
+        f"{task_dir}/compression_methods_qualitative_comparison.pdf",
+        format="pdf",
+        dpi=600,
+    )
+    plt.show()
+
+def plot_individual_figs(title=False):
+    plt.figure()
+    for i, bbp in enumerate(selected_bpps):
+        for j, method in enumerate(selected_methods):
+            ii = np.argmin(np.abs(np.array(bpps[method]) - bbp))
+            reconstructed_image = reconstructed_images[method][ii]
+            real_bpp = bpps[method][ii]
+            plt.imshow(reconstructed_image.permute(1, 2, 0))
+            if title:
+                plt.title(method, fontsize=24)
+            plt.axis("off")
+            plt.savefig(
+                f"{task_dir}/{task_name}_{method}_bpp_{real_bpp:.3f}.pdf",
+                format="pdf",
+                dpi=600,
+            )
+
+def save_metadata():
+    metadata = {"psnr_values": psnr_values, "ssim_values":ssim_values, "bpps": bpps}
+
+    with open(f"{task_dir}/{task_name}_metadata.json", 'w') as json_file:
+        json.dump(metadata, json_file, indent=4)
+
+
+plot_individual_figs()
+save_metadata()
